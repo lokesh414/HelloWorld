d498ca99f3e27e2a01b6f7bf84c1a3b141087767
lokesh414 <lokeshisq97@gmail.com>
2021-06-23 14:37:27 +0530

    Hello

--- MyFirstProject.java
+++ MyFirstProject.java
@@ -1,8 +1,10 @@
-import.java.io.*;
-class MyFirstProject
-{
-public static void main(String args[])
-{
-System.out.println("hello world");
-}
-}
+public class Main {
+  int x = 5;
+int y = 10;
+  public static void main(String[] args) {
+    Main myObj1 = new Main();
+	Main myObj2 = new Main();
+    System.out.println(myObj.x);
+	
+  }
+
